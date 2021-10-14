40b0cf66932accf47398a9161d7c72955b47a1e8
marloncoti@gmail.com <marloncoti@gmail.com>
2021-10-14 16:47:45 -0600

    feat(cicd) added env attribute

--- bash.sh
+++ bash.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# VERSION=''
+
+# tag="v1.0.0.0"
+
+# preffix=${tag:0:1}
+
+# if [ "${preffix,,}" == "v" ]; then VERSION="${tag:1}"; else VERSION="${tag:0}"; fi
+
+# echo $VERSION
+
+
+TAG_NAME='v1343.1203'
+TAG=''
+PREFIX=''
 VERSION=''
 
-tag="v1.0.0.0"
+if [ -z "$TAG_NAME" ]; then TAG="1.0.0"; else TAG=${TAG_NAME}; fi
 
-preffix=${tag:0:1}
-
-if [ "${preffix,,}" == "v" ]; then VERSION="${tag:1}"; else VERSION="${tag:0}"; fi
-
-echo $VERSION
+PREFIX=${TAG:0:1}
+if [ "${PREFIX,,}" == "v" ]; then VERSION="${TAG:1}"; else VERSION="${TAG:0}"; fi
+echo $VERSION 
